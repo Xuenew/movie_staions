bfd70d03ba9ad0b448286ba8670ddb5916238697
Xuenew <41135035+Xuenew@users.noreply.github.com>
2019-03-31 18:33:05 +0800

    Update get_movie_new.py

--- get_movie_new.py
+++ get_movie_new.py
@@ -1,7 +1,7 @@
 #Author:xue yi yang
 import requests
 import re
-from lxml import etree
+#from lxml import etree
 import html
 import json
 class serch_movies_new_big():
@@ -26,10 +26,31 @@ class serch_movies_new_big():
         self.data = {
             "adparam": "pf=in&ad_type=LD%7CKB%7CPVL&pf_ex=pc&url=https%3A%2F%2Fv.qq.com%2Fx%2Fcover%2Ffgqtuu38z91hfyw%2Fh0027agjqb1.html&refer=https%3A%2F%2Fv.qq.com%2Fx%2Fcover%2Ffgqtuu38z91hfyw%2Fh0027agjqb1.html&ty=web&plugin=1.0.0&v=3.5.57&coverid=fgqtuu38z91hfyw&vid=h0027agjqb1&pt=&flowid=cd7021b1f780f9822a630a4ea6863993_10201&vptag=www_baidu_com&pu=-1&chid=0&adaptor=2&dtype=1&live=0&resp_type=json&guid=c3ed01ec03a21050db27858ad6cd8f20&req_type=1&from=0&appversion=1.0.139&uid=241119669&tkn=47730947ee6695f300000000f0802b89a3468aa32c62&lt=qq&platform=10201&opid=96A774A2ACC02EBD09BFB659E5419832&atkn=17E8253E83CDCF818F97F93068F55F23&appid=101483052&tpid=1&rfid=11bb3527365ee03b120933b36539c21b_1550995743"
             ,"buid": "vinfoad"
-            ,"vinfoparam":"charge=1&defaultfmt=auto&otype=ojson&guid=c3ed01ec03a21050db27858ad6cd8f20&flowid=cd7021b1f780f9822a630a4ea6863993_10201&platform=10201&sdtfrom=v1010&defnpayver=1&appVer=3.5.57&host=v.qq.com&ehost=https%3A%2F%2Fv.qq.com%2Fx%2Fcover%2Ffgqtuu38z91hfyw%2Fh0027agjqb1.html&refer=v.qq.com&sphttps=1&tm=1551083974&spwm=4&logintoken=%7B%22main_login%22%3A%22qq%22%2C%22openid%22%3A%2296A774A2ACC02EBD09BFB659E5419832%22%2C%22appid%22%3A%22101483052%22%2C%22access_token%22%3A%2217E8253E83CDCF818F97F93068F55F23%22%2C%22vuserid%22%3A%22241119669%22%2C%22vusession%22%3A%2247730947ee6695f300000000f0802b89a3468aa32c62%22%7D&unid=7d523775a45e11e89d19a0429186d00a&vid={}&defn=&fhdswitch=0&show1080p=1&isHLS=1&dtype=3&sphls=2&spgzip=1&dlver=2&drm=32&hdcp=0&spau=1&spaudio=15&defsrc=1&encryptVer=8.1&cKey={}&fp2p=1&spadseg=1".format(self.vid,self.ck)
+            ,"vinfoparam":"charge=1&defaultfmt=auto&otype=ojson&guid=c3ed01ec03a21050db27858ad6cd8f20&flowid=cd7021b1f780f9822a630a4ea6863993_10201&platform=10201&sdtfrom=v1010&defnpayver=1&appVer=3.5.57&host=v.qq.com&ehost=https%3A%2F%2Fv.qq.com%2Fx%2Fcover%2Ffgqtuu38z91hfyw%2Fh0027agjqb1.html&refer=v.qq.com&sphttps=1&tm=1551083974&spwm=4&logintoken=%7B%22main_login%22%3A%22wx%22%2C%22openid%22%3A%22ox8XOvie1TjyS7CvgWw4vKcr0jDM%22%2C%22appid%22%3A%22wx5ed58254bc0d6b7f%22%2C%22access_token%22%3A%2220_3U4PxhLQSD2lneF775TjZiV9CaNCnUAN5ZycmYMJewJRp9YJqGL8Zyxz0U5GURObsaq3ttQBwj8N00prOrKA6w%22%2C%22vuserid%22%3A%221363393582%22%2C%22vusession%22%3A%22{}%22%7D&unid=7d523775a45e11e89d19a0429186d00a&vid={}&defn=&fhdswitch=0&show1080p=1&isHLS=1&dtype=3&sphls=2&spgzip=1&dlver=2&drm=32&hdcp=0&spau=1&spaudio=15&defsrc=1&encryptVer=8.1&cKey={}&fp2p=1&spadseg=1".format(self.get_vusession(),self.vid,self.ck)
 
         }
         # self.proxies = {'http': '114.249.116.88:9000',}  ,proxies = self.proxies
+    def get_vusession(self):
+        #print("ok")
+        headers = {
+            "user-agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36",
+"cookie": "pgv_pvi=6886795264; RK=gXTYpKgRwG; ptcz=840f92f9f159c3df7426ee93de8db40f2aac5846024c670da50da72b38a86f57; tvfe_boss_uuid=262d9f7357466816; pgv_pvid=8810885929; eas_sid=c1g5k3c8d5z7F5C4i3B53815G4; _ga=GA1.2.1401926032.1538576269; pac_uid=1_1287986063; _video_qq_version=1.1; _video_qq_appid=wx5ed58254bc0d6b7f; _video_qq_openid=ox8XOvie1TjyS7CvgWw4vKcr0jDM; _video_qq_vuserid=1363393582; _video_qq_main_login=wx; appid=wx5ed58254bc0d6b7f; openid=ox8XOvie1TjyS7CvgWw4vKcr0jDM; vuserid=1363393582; main_login=wx; refresh_token=19_zd-NrgOjiSc5Osv-JRPAtz25OAW2a5HtjfZtu4mJ5_rzMyk2RO47Wlmj5-4-gYEZaQ6qK9Fl6H3_do2XYVFd9g; next_refresh_time=2592000; _video_qq_refresh_token=19_zd-NrgOjiSc5Osv-JRPAtz25OAW2a5HtjfZtu4mJ5_rzMyk2RO47Wlmj5-4-gYEZaQ6qK9Fl6H3_do2XYVFd9g; _video_qq_next_refresh_time=2592000; access_token=20_3U4PxhLQSD2lneF775TjZiV9CaNCnUAN5ZycmYMJewJRp9YJqGL8Zyxz0U5GURObsaq3ttQBwj8N00prOrKA6w; _video_qq_access_token=20_3U4PxhLQSD2lneF775TjZiV9CaNCnUAN5ZycmYMJewJRp9YJqGL8Zyxz0U5GURObsaq3ttQBwj8N00prOrKA6w; pgv_info=ssid=s6327553603; login_time_last=2019-3-31 16:6:6; vusession=3zPPo73MVjep9xIu5bpwqQ..; login_time_init=1554020711; _video_qq_vusession=3zPPo73MVjep9xIu5bpwqQ..; _video_qq_login_time_init=1554020711"
+        ,"upgrade-insecure-requests": "1"
+        }
+#         data = {
+#             "vappid": "11059694",
+# "vsecret": "fdf61a6be0aad57132bc5cdf78ac30145b6cd2c1470b0cfe",
+# "type": "wx",
+# "callback": "jQuery191022418280920547407_1554017162095",
+# "_": "1554017162096"
+#         }
+        res = requests.get(url="https://access.video.qq.com/user/auth_refresh?vappid=11059694&vsecret=fdf61a6be0aad57132bc5cdf78ac30145b6cd2c1470b0cfe&type=wx&callback=jQuery191022418280920547407_1554017162095&_=1554017162096",headers=headers,).text#verify=False
+        #print(res)
+        vuserssion = re.findall(r'{"(.*)"}',res)[0]
+        vuserssion_json = json.loads('{"'+vuserssion+'"}')["vusession"]
+        #print(vuserssion_json)
+        return vuserssion_json
+		
     def get_paly_url(self):
         res = requests.get(url="https://vd.l.qq.com/proxyhttp",headers=self.head, data=self.data).text
         # print(json.loads(res)["vinfo"])
@@ -43,11 +64,13 @@ class serch_movies_new_big():
         # print(x["vl"]["vi"][0]["ul"]["ui"][0]["hls"]["pt"])
         # print(json.loads(res)["vinfo"]["vl"]["vi"][0]["ul"]["ui"][0]["url"])
         # print(json.loads(res)["vinfo"]["vl"]["vi"][0]["ul"]["ui"][0]["hls"]["pt"])
-        # print(x)
+        # print(x) 可以打开看看手机浏览和电脑浏览的区别 手机端浏览缺少v1
         play_url = x["vl"]["vi"][0]["ul"]["ui"][0]["url"] + x["vl"]["vi"][0]["ul"]["ui"][0]["hls"]["pt"]
         # print(play_url)
         return play_url
         # return x
 if __name__ == '__main__':
     s = serch_movies_new_big(ckey="0284615FC2D67889B822380F9B078E47A35354F764767C4CA3AE8255DB3FF795AAAFA601FF23696F65071FA4E7F21749DFA5A62B9EBC193809E1D9B34343CBCB59AFC14FCF5B512C9823CB27DEA615FDF9A33D8082628E30456E218D1A15B871EEB843617DF7AC32B05B3FACB5DA36C7327C509CB23FB91C0296356E3C0519E3AE739FBC8D22E319BDF8C7EB6A5FA9CB2A8B7D451CF4AC56203D9404A413C440B9413ADE57C9003202D9C6728EF7095CCDDABB194FAA86545F3E1F6FB333E5643B8FADED05B2A024B148EAF936ACA47BA0D38A0812394450EF1351058768B305",vid="h0027agjqb1")
-    s.get_paly_url()
+    #s.get_paly_url()
+    print(s.data)
+    #s.get_vusession()
